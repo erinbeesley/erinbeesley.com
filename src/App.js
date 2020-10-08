9daa0406eefbe037bdd92cf238fede4e0b5a9608
erin beesley <erinbeesley958@gmail.com>
2020-10-08 19:44:02 -0400

    Change to React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles/main.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className="text-teal-600 text-6xl">IF YOU'RE SEEING THIS MY WEBSITE IS UP AND RUNNING WOOHOO</h1>
+
+    </div>
+  );
+}
+
+export default App;
